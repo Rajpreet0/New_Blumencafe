3a34a3defcc0fe31301fdecb26dd032f0e5c9131
Rajpreet0 <rajpreet.singh@gmx.de>
2023-03-08 19:54:41 +0100

    bug fixes third try

--- components/Contact.js
+++ components/Contact.js
@@ -52,7 +52,7 @@ const Contact = () => {
 
 
                 <form
-                onSubmit={sendEmail}
+                //onSubmit={sendEmail}
                 className='w-full max-w-lg'>
                   <div className='flex flex-wrap -mx-3 mb-4'>
 
