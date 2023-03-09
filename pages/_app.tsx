a8edf8e72d230436ef818695fef9ec36ec859b6b
Rajpreet0 <rajpreet.singh@gmx.de>
2023-03-09 10:56:34 +0100

    Added Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '../components/Header'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return ( <>
+    
+   <Script 
+   strategy='lazyOnload'
+   src={`https://www.googletagmanager.com/gtag/js?id=G-RJH84F2XC7`}/>
+
+   <Script strategy='lazyOnload'>
+     {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-RJH84F2XC7');
+     `}
+   </Script>
+
     <Header/>
     <Component {...pageProps} />
-
+ 
   </>
   )
 }
